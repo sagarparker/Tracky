ade89c7465eef75418e5cfdc1a227b97123d2e94
sagarparker <sagar8parker@gmail.com>
2024-07-12 16:23:37 +0530

    Fix for nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -16,7 +16,7 @@ function Nav() {
         </a>
         {
             session ? 
-            <button onClick={()=>signOut()} className="flex justify-between items-center text-white bg-black hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium text-sm px-3 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 rounded-full">
+            <button onClick={()=>signOut()} className="flex justify-between items-center text-white bg-transparent font-semibold text-l focus:outline-none focus:ring-4 focus:ring-gray-300 px-3 py-2.5 mb-2 ">
             <img src={img_src!} height={30} width={30} className="rounded-full mr-2"/>
             Logout
             </button>:null
